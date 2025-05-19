d8d58016f512c497c1775cab42603e5d7ec144cf
meiwata <waleilulu@gmail.com>
2025-05-19 19:26:41 +0800

    記憶體位置

--- ch07/Array1.cpp
+++ ch07/Array1.cpp
@@ -18,8 +18,12 @@ int main()
 	int Ary[]= {10, 20, 30};
 	cout<<"Ary的記憶體位置: "<< Ary << endl;  // 0x6ffe10
 	cout<<"Ary[0]的值: "<< Ary[0] << endl; //10 
-	 cout<<"Ary[0]的記憶體位置: "<< &Ary[0] << endl; // 0x6ffe10
-	 
+	cout<<"Ary[0]的記憶體位置: "<< &Ary[0] << endl; // 0x6ffe10
+	cout<<"Ary[1]的記憶體位置: "<< &Ary[1] << endl; // 0x6ffe04
+	cout<<"Ary[2]的記憶體位置: "<< &Ary[2] << endl; // 0x6ffe08
+	/*數字是 4 個byte*/
+	
+	
 	cout << "一.Ary陣列傳址:" << endl;
     ShowAry(Ary);  // 10 20 30
     cout << "二.Ary陣列當參數 傳址後" << endl;
