ddeb9365dfd31bfb5ee1c31091910946d038636c
meiwata <waleilulu@gmail.com>
2025-05-19 20:39:19 +0800

    變數的內容、記憶體位址、記憶體大小

--- ch08/Pointer1.cpp
+++ ch08/Pointer1.cpp
@@ -4,12 +4,12 @@ int main()
 {
     double m=5;
 	int n=10;
-	cout << "變數 m 的值=" << m << endl ;
-	cout << "變數 n 的值=" << n << endl ;
-	cout << "變數 m 的位址=" << &m <<endl;
-	cout << "變數 n 的位址=" << &n <<endl;
-	cout << "變數 m 的記憶體=" << sizeof(m) << "Bytes" << endl;
-	cout << "變數 n 的記憶體=" << sizeof(n) << "Bytes" << endl;
+	cout << "變數 m 的內容=" << m << endl ;
+	cout << "變數 n 的內容=" << n << endl ;
+	cout << "變數 m 的記憶體位址=" << &m <<endl;
+	cout << "變數 n 的記憶體位址=" << &n <<endl;
+	cout << "變數 m 的記憶體大小=" << sizeof(m) << " Bytes" << endl;
+	cout << "變數 n 的記憶體大小=" << sizeof(n) << " Bytes" << endl;
 	system("pause");
 	return 0;
 }
