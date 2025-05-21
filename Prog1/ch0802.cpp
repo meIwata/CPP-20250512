bdee1ec92acbc7c3ca247b1bd20eb5898d009a65
meiwata <waleilulu@gmail.com>
2025-05-21 19:32:15 +0800

    for迴圈

---
+++ Prog1/ch0802.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+
+int main(){
+	int var[] = {7, 5, 2, 1};
+	int sum = 0;
+	for(int i = 0; i<4; i++){
+		sum += var[i];
+	}
+	printf("¥[Á`: %d%n", sum);
+
+	system("PAUSE");
+	return 0;
+}
+
