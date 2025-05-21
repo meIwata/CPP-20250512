38175f269fdb1046143edea2acd0f9726fdf3781
meiwata <waleilulu@gmail.com>
2025-05-21 20:54:15 +0800

    Call by value(值)

---
+++ Prog1/byValue.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+using namespace std;
+// 自訂函式宣告
+void swap(int x, int y);
+
+int main(){
+	int a = 10, b = 20;
+	printf("a: %3d, b: %3d\n", a, b);
+	swap(a, b);
+	printf("a: %3d, b: %3d\n", a, b);
+
+	system("PAUSE");
+	return 0;
+}
+
+// 自訂函式實作
+void swap(int x, int y){  
+	int temp = x;
+	x = y;
+	y = temp;
+}
+
