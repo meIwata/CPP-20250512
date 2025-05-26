8398a3e1a649dbdb545617afcb905179f114d493
meiwata <waleilulu@gmail.com>
2025-05-26 20:02:20 +0800

    指標變數指向記憶體內容

--- Prog1/ch0803.cpp
+++ Prog1/ch0803.cpp
@@ -6,8 +6,11 @@ int main(){
 
 	int *p1 = new int; // 宣告一個指標變數 
 	*p1 = 10;
-	
 	printf("指標p1指向記憶體內容: %d\n", *p1); 
+	
+	int *p2 = new int(30);
+	printf("指標p2指向記憶體內容: %d\n", *p2); 
+	
 	system("PAUSE");
 	return 0;
 }
