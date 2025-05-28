93ee6047371ba37737805f3d4e885f40296ffcec
meiwata <waleilulu@gmail.com>
2025-05-28 20:02:27 +0800

    讀取檔案內容，並將每一行存進 vector，再逐一輸出

--- ch09_forC++file/fread1/fread4.cpp
+++ ch09_forC++file/fread1/fread4.cpp
@@ -1,11 +1,14 @@
 #include <iostream>
 #include <fstream>
 #include <string.h>
+#include <vector>
 using namespace std;
 int main() 
 {
     fstream file;  	  //建立檔案輸出、輸入物件	
-    char buffer[100]; //C傳統字串
+    
+    vector<string> v1;
+    
     string str; //C++ 字串 
     int count=0;
     file.open("test03.txt", ios::in); //開啟檔案
@@ -15,16 +18,21 @@ int main()
     {
         do 
         {
-//            file.getline(buffer,sizeof(buffer)); // 每次讀取一行
+
 			getline(file,str);
-//            cout << buffer << "-- C --" << strlen(buffer) << endl;
-//            str = buffer; //c傳統字串 轉成 c++ 字串物件 
+			v1.push_back(str);			
+			
             cout << str << "-- C++ --" << str.length() << endl;  
             count++;
         } while(!file.eof());  // 是否至檔案結尾 (end of file)
         file.close(); //關閉檔案
     }
     cout << "count:" << count <<endl; 
+    printf("\nVector element list\n");
+    for(int i=0; i<v1.size(); i++){
+    	printf("%d: %s\n", i+1, v1.at(i).c_str());
+	}
+    
     system("pause");
     return 0;
 } 
